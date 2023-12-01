0ba508f4f83a83c0bfd2a19320ee2a1b147a0458
pibragimov <pibragimov@whoosh.bike>
2023-12-01 19:10:32 +0300

    auto: autocommit

--- linux/new_server.sh
+++ linux/new_server.sh
@@ -8,6 +8,7 @@ apt install -y python3 python3-pip python3-dev python3-setuptools python3-venv h
 cd /tmp
 git clone https://gitlab.com/teadove/dotfiles
 cd dotfiles
-./dotfiles_setup.py -r
+./dotfiles_setup.py -f
+cd ~
 
 chsh -s $(which zsh)
