f98d0e376a1cd6262e9463e6dcd16329243b8550
TeaDove <peter.ibragimov@gmail.com>
2026-02-05 20:46:14 +0400

    evening small edit from TeaDoves-MacBook-Air-M4.local

---
+++ services/nextcloud/autoloader/go.mod
@@ -0,0 +1,28 @@
+module autoloader
+
+go 1.25
+
+require (
+	github.com/cockroachdb/errors v1.12.0
+	github.com/emersion/go-webdav v0.7.0
+	github.com/rs/zerolog v1.34.0
+	github.com/teadove/teasutils/service_utils v0.1.2
+)
+
+require (
+	github.com/caarlos0/env/v11 v11.3.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20241215232642-bb51bb14a506 // indirect
+	github.com/cockroachdb/redact v1.1.6 // indirect
+	github.com/getsentry/sentry-go v0.41.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/teadove/teasutils/utils v0.3.3 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+)
---
+++ services/nextcloud/autoloader/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/cockroachdb/errors"
+	"github.com/emersion/go-webdav"
+	"github.com/rs/zerolog"
+	"github.com/teadove/teasutils/service_utils/logger_utils"
+)
+
+const (
+	path   = "https://pi.lan/remote.php/dav/files/%s/"
+	dstDir = "autoloader"
+)
+
+func run(ctx context.Context, username, password string) error {
+	client, err := webdav.NewClient(webdav.HTTPClientWithBasicAuth(&http.Client{}, username, password), fmt.Sprintf(path, username))
+	if err != nil {
+		return errors.Wrap(err, "creating webdav client")
+	}
+
+	_, err = client.Stat(context.Background(), dstDir)
+	if err != nil {
+		err = client.Mkdir(ctx, dstDir)
+		if err != nil {
+			return errors.Wrap(err, "creating directory")
+		}
+	}
+
+	err = filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if info.Size() == 0 {
+			return nil
+		}
+
+		logger := zerolog.Ctx(ctx).With().Str("path", path).Logger()
+
+		logger.Info().Msg("uploading.file")
+
+		dst := filepath.Join(dstDir, info.Name())
+
+		_, err = client.Stat(ctx, dst)
+		if err == nil {
+			logger.Info().Msg("already.exists")
+			return nil
+		}
+
+		writer, err := client.Create(ctx, dst)
+		if err != nil {
+			return errors.Wrap(err, "creating file")
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return errors.Wrap(err, "opening file")
+		}
+
+		_, err = file.WriteTo(writer)
+		if err != nil {
+			return errors.Wrap(err, "copying file")
+		}
+
+		err = writer.Close()
+		if err != nil {
+			return errors.Wrap(err, "closing file")
+		}
+
+		logger.Info().Msg("uploaded")
+
+		return nil
+	})
+	if err != nil {
+		return errors.Wrap(err, "walking directory")
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Printf("Enter username: ")
+	var (
+		username string
+		password string
+	)
+
+	_, err := fmt.Scanln(&username)
+	if err != nil {
+		fmt.Printf("%s\n", errors.Wrap(err, "failed to read username"))
+	}
+
+	fmt.Printf("Enter password: ")
+	_, err = fmt.Scanln(&password)
+	if err != nil {
+		fmt.Printf("%s\n", errors.Wrap(err, "failed to read password"))
+	}
+
+	err = run(logger_utils.NewLoggedCtx(), username, password)
+	if err != nil {
+		fmt.Printf("%s\n", errors.Wrap(err, "failed to run autoloader"))
+	}
+}
